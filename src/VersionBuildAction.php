02c2adecbf3aed0c223edce6c86485c6f2c3f5d6
Vortechstudio <contact@vortechstudio.fr>
2024-04-28 14:24:55 +0200

    fix(Core): Correction de l'affichage des builds

--- src/VersionBuildAction.php
+++ src/VersionBuildAction.php
@@ -2,6 +2,8 @@
 
 namespace Vortechstudio\VersionBuildAction;
 
+use Illuminate\Support\Facades\Cache;
+
 class VersionBuildAction
 {
     private mixed $api_key;
@@ -35,25 +37,41 @@ class VersionBuildAction
 
     private  function getLastTag()
     {
+        $cacheKey = 'gh_last_tag_'.$this->owner.'_'.$this->repository;
+        $ttl = 30;
+
+        if(Cache::has($cacheKey)) {
+            return Cache::get($cacheKey);
+        }
+
         $response = \Http::withToken($this->api_key)
             ->get('https://api.github.com/repos/'.$this->owner.'/'.$this->repository.'/releases/latest');
 
         if($response) {
+            Cache::put($cacheKey, $response['tag_name'], $ttl);
             return $response['tag_name'];
         } else {
-            return "0.0.0";
+            return "Erreur lors de la recuperation du dernier tag";
         }
     }
 
     private function getLastCommitHash()
     {
+        $cacheKey = 'gh_last_commit_'.$this->owner.'_'.$this->repository;
+        $ttl = 30;
+
+        if(Cache::has($cacheKey)) {
+            return Cache::get($cacheKey);
+        }
+
         $response = \Http::withToken($this->api_key)
             ->get('https://api.github.com/repos/'.$this->owner.'/'.$this->repository.'/commits/master');
 
         if($response) {
+            Cache::put($cacheKey, $response['sha'], $ttl);
             return substr($response['sha'], 0, 7);
         } else {
-            return "null";
+            return "Erreur lors de la recuperation du dernier commit";
         }
     }
 }
